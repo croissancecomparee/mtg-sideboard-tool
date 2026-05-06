7c575b353cec7fb6157a89f24ef342e3653a6429
croissancecomparee <gaetan.cece.90@gmail.com>
2026-05-06 16:05:45 +0200

    add the possibility to select the format and the decklist

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import argparse
 from src.parser import parse_decklist
 from src.generator import generate_sideboard_matrix
 from src.exporter import export_to_excel
 
-if __name__ == "__main__":
-    decklist = parse_decklist("deck.txt")
-    print(decklist)
-    sideboard_matrix = generate_sideboard_matrix(decklist, "modern")
-    export_to_excel(sideboard_matrix)
-    # print("MAINBOARD")
-    # for card in decklist["mainboard"]:
-    #     print(f"{card['quantity']} {card['name']}")
-    #     # print()
+def main():
+    parser = argparse.ArgumentParser(description="MTG Sideboard Matrix Generator")
+    parser.add_argument("--format", type=str, required=True, help="Deck Format (e.g., Standard, Modern, Legacy)")
+    parser.add_argument("--decklist", type=str, default="deck.txt", help="fichier decklist (format texte)")
 
-    # print("\nSIDEBARD")
-    # for card in decklist["sideboard"]:
-    #     print(f"{card['quantity']} {card['name']}")
+    args = parser.parse_args()
+
+    print(f"📦 Format choisi : {args.format}")
+    print(f"📄 Deck input : {args.decklist}")
+
+    decklist = parse_decklist(args.decklist)
+    sideboard_matrix = generate_sideboard_matrix(decklist, args.format)
+    export_to_excel(sideboard_matrix)
+
+if __name__ == "__main__":
+    main()
