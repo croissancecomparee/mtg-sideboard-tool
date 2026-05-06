41c1b16b41073f9facaaa11789cf9b8031142093
croissancecomparee <gaetan.cece.90@gmail.com>
2026-05-06 15:38:01 +0200

    update the matrix generator

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from src.parser import parse_decklist
-from src.generator import generate_sideboard_table
+from src.generator import generate_sideboard_matrix
 from src.exporter import export_to_excel
 
 if __name__ == "__main__":
     decklist = parse_decklist("deck.txt")
     print(decklist)
-    sideboard_table = generate_sideboard_table(decklist, "modern")
-    export_to_excel(sideboard_table)
+    sideboard_matrix = generate_sideboard_matrix(decklist, "modern")
+    export_to_excel(sideboard_matrix)
     # print("MAINBOARD")
     # for card in decklist["mainboard"]:
     #     print(f"{card['quantity']} {card['name']}")
