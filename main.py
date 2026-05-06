7607ddabfe321476cb44408b3d8ad2e9a6c2ab86
croissancecomparee <gaetan.cece.90@gmail.com>
2026-05-06 15:08:19 +0200

    add export

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from src.parser import parse_decklist
+from src.generator import generate_sideboard_table
+from src.exporter import export_to_excel
 
 if __name__ == "__main__":
     decklist = parse_decklist("deck.txt")
     print(decklist)
+    sideboard_table = generate_sideboard_table(decklist, "modern")
+    export_to_excel(sideboard_table)
+    # print("MAINBOARD")
+    # for card in decklist["mainboard"]:
+    #     print(f"{card['quantity']} {card['name']}")
+    #     # print()
 
-    print("MAINBOARD")
-    for card in decklist["mainboard"]:
-        print(f"{card['quantity']} {card['name']}")
-        # print()
-
-    print("\nSIDEBARD")
-    for card in decklist["sideboard"]:
-        print(f"{card['quantity']} {card['name']}")
+    # print("\nSIDEBARD")
+    # for card in decklist["sideboard"]:
+    #     print(f"{card['quantity']} {card['name']}")
