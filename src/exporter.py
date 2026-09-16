7fc19202d23f5e59cfd586bf9f473bc6d3d4e65b
croissancecomparee <gaetan.cece.90@gmail.com>
2026-09-16 17:43:14 +0200

    update path for config loading, and change the export message

--- src/exporter.py
+++ src/exporter.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+
+
 def export_to_excel(df, file_name="sideboard_plan.xlsx"):
     '''
     fonction qui exporte une table de sideboard dans un fichier excel
     '''
     df.to_excel(file_name, index=False)
     print(f"✅ Fichier généré : {file_name}")
+
+    # output_path = Path(file_name)
+
+    # df.to_excel(output_path, index=False)
+
+    # return output_path
