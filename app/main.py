23d2cb40674d6a055f00521f1c1cf21ff9b67122
croissancecomparee <gaetan.cece.90@gmail.com>
2026-09-16 18:48:16 +0200

    add api list formats

--- app/main.py
+++ app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from src.config_loader import load_formats_config
 
 app = FastAPI(title="MTG Sideboard Matrix Generator", description="API pour générer des matrices de sideboard pour Magic: The Gathering", version="1.0.0")
 
 @app.get("/")
 def home():
     return {"message": "Bienvenue sur l'API MTG Sideboard Matrix Generator. Utilisez les endpoints pour générer des matrices de sideboard."}
+
+@app.get("/formats")
+def get_formats():
+    return load_formats_config()
